ac39445ff0424e80cdba4d072d2142dd93129996
midoks <midoks@163.com>
2023-05-10 17:46:00 +0800

    up

--- data/sql/default.sql
+++ data/sql/default.sql
@@ -11,3 +11,11 @@ CREATE TABLE IF NOT EXISTS `users` (
 INSERT INTO `users` (`id`, `username`, `password`, `login_ip`, `login_time`, `phone`, `email`) VALUES
 (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', '192.168.0.10', '2022-02-02 00:00:00', 0, 'midoks@163.com');
 
+
+
+CREATE TABLE IF NOT EXISTS `tg_bot` (
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `alias` TEXT, 
+  `token` TEXT,
+  `email` TEXT
+);
