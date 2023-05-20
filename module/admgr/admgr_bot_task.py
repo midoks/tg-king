08bd57de9815fd04b9f373fd13f303d41131be0b
midoks <midoks@163.com>
2023-05-20 10:20:11 +0800

    up

--- module/admgr/admgr_bot_task.py
+++ module/admgr/admgr_bot_task.py
@@ -15,16 +15,37 @@ import tgking
 
 import telebot
 
+bot = telebot.TeleBot(cfg['bot']['app_token'])
+
+
+def writeLog(log_str):
+    if __name__ == "__main__":
+        print(log_str)
+
+    now = tgking.getDateFromNow()
+    log_file = getServerDir() + '/admgr.log'
+    tgking.writeLog(now + ':' + log_str, log_file, limit_size=5 * 1024)
+    return True
+
+
+def runBotPushTask():
+    plist = getStartExtCfgByTag('push')
+    for p in plist:
+        try:
+            script = p['name'].split('.')[0]
+            __import__(script).run(bot)
+        except Exception as e:
+            writeLog('-----runBotPushTask error start -------')
+            writeLog(tgking.getTracebackInfo())
+            writeLog('-----runBotPushTask error end -------')
+
+
+def botPush():
+    while True:
+        runBotPushTask()
+        time.sleep(1)
 
 if __name__ == "__main__":
-
     # 机器人推送任务
     botPushTask = threading.Thread(target=botPush)
     botPushTask.start()
-
-    # 机器人其他推送任务
-    botPushOtherTask = threading.Thread(target=botPushOther)
-    botPushOtherTask.start()
-
-    writeLog('启动成功')
-    runBot(bot)
