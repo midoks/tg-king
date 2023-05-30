8715373468cb35a452299f99e47137aca0b04fba
midoks <midoks@163.com>
2023-05-30 14:51:01 +0800

    Update model.py

--- module/gpmgr/class/model.py
+++ module/gpmgr/class/model.py
@@ -7,3 +7,12 @@ import orm
 
 sys.path.append(os.getcwd() + "/class/core")
 import tgking
+
+
+def unameMosaic(uname):
+    '''
+    对用户名打马赛克
+    '''
+    if len(uname) > 4:
+        return '%s██%s' % (uname[0:2], uname[-2:])
+    return '%s██%s' % (uname[0:1], uname[-1:])
