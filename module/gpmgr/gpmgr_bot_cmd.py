03bc0751cad8a733d3e45b683b9acf8ff8da6c1a
midoks <midoks@163.com>
2023-05-31 22:42:17 +0800

    Update gpmgr_bot_cmd.py

--- module/gpmgr/gpmgr_bot_cmd.py
+++ module/gpmgr/gpmgr_bot_cmd.py
@@ -85,8 +85,16 @@ def hanle_ban(message):
     if message.chat.type != 'supergroup':
         return True
 
-    # writeLog('ban:' + str(message))
-    # bot.ban_chat_member(message, message.chat.id)
+    try:
+        if hasattr(message, 'reply_to_message'):
+            data = bot.get_chat_member(message.chat.id, message.from_user.id)
+            if data['status'] in ('administrator', 'creator'):
+                bot.ban_chat_member(
+                    message.chat.id, message.reply_to_message.json.from.id)
+        else:
+            bot.answer_callback_query(message.id, text='你无权操作!')
+    except Exception as e:
+        writeLog(str(e))
 
 
 @bot.callback_query_handler(func=lambda call: True)
